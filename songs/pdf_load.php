295057744366d29a34cfbc39044ba83fb539b2ce
Josh Deese <joshmdeese@gmail.com>
2013-03-11 15:01:48 -0400

    Initial Commit

--- songs/pdf_load.php
+++ songs/pdf_load.php
@@ -4,6 +4,7 @@
 		header('Content-disposition: inline; filename='.$_GET['id'].'.pdf');
 		@readfile('../content/pdf/'.$_GET['type'].'/'.$_GET['id'].'.pdf');
 	} else {
+		// todo: this is where users can upload new content
 		echo "This file doesn't yet exist";
 	}
 ?>
